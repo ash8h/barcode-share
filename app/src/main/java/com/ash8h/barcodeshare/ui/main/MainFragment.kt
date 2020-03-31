016af8e606316fae3dc3bbfedf17aa9af7fbad3f
Atsushi HASHIZUME <atsushi.hashizume@origami.com>
2020-04-01 02:32:21 +0900

    add FAB to add barcode

--- app/src/main/java/com/ash8h/barcodeshare/ui/main/MainFragment.kt
+++ app/src/main/java/com/ash8h/barcodeshare/ui/main/MainFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -21,7 +23,23 @@ class MainFragment : Fragment() {
         fun newInstance() = MainFragment()
     }
 
+    private enum class FabState { OPEN, CLOSE }
+
     private val viewModel: MainViewModel by viewModel()
+    private var fabState: FabState = FabState.CLOSE
+    private val fabOpenAnimation: Animation by lazy {
+        AnimationUtils.loadAnimation(requireContext(), R.anim.fab_open)
+    }
+    private val fabCloseAnimation: Animation by lazy {
+        AnimationUtils.loadAnimation(requireContext(), R.anim.fab_close)
+    }
+    private val fabRotateClockwise: Animation by lazy {
+        AnimationUtils.loadAnimation(requireContext(), R.anim.fab_rotate_clock)
+    }
+    private val fabRotateCounterClockwise: Animation by lazy {
+        AnimationUtils.loadAnimation(requireContext(), R.anim.fab_rotate_anticlock)
+    }
+
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         return inflater.inflate(R.layout.fragment_main, container, false)
@@ -34,12 +52,45 @@ class MainFragment : Fragment() {
         recycler_view.adapter = adapter
         recycler_view.layoutManager = LinearLayoutManager(requireContext())
 
+        fab.setOnClickListener {
+            when (fabState) {
+                FabState.OPEN -> closeFab()
+                FabState.CLOSE -> openFab()
+            }
+        }
+        fab_camera.setOnClickListener { }
+        fab_library.setOnClickListener { }
+
         viewModel.barcodes.observeNotNull(this) { barcodes ->
             adapter.items = barcodes
             adapter.notifyDataSetChanged()
             empty.visibility = if (barcodes.isEmpty()) View.VISIBLE else View.GONE
         }
         viewModel.fetchBarcodes()
+    }
+
+    private fun openFab() {
+        fab_background.visibility = View.VISIBLE
+        fab_camera_text.visibility = View.VISIBLE
+        fab_library_text.visibility = View.VISIBLE
+        fab_camera.visibility = View.VISIBLE
+        fab_library.visibility = View.VISIBLE
+        fab_camera.startAnimation(fabOpenAnimation)
+        fab_library.startAnimation(fabOpenAnimation)
+        fab.startAnimation(fabRotateClockwise)
+        fabState = FabState.OPEN
+    }
+
+    private fun closeFab() {
+        fab_background.visibility = View.GONE
+        fab_camera_text.visibility = View.GONE
+        fab_library_text.visibility = View.GONE
+        fab_camera.visibility = View.GONE
+        fab_library.visibility = View.GONE
+        fab_camera.startAnimation(fabCloseAnimation)
+        fab_library.startAnimation(fabCloseAnimation)
+        fab.startAnimation(fabRotateCounterClockwise)
+        fabState = FabState.CLOSE
     }
 
     private class BarcodeRecyclerViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
